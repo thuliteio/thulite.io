9690fe5bb614afb93c860cabc53140c6b7f2a946
Henk Verlinde <henk@ventizo.com>
2020-01-06 11:18:57 +0100

    Update for UX

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,9 +1,26 @@
 const autoprefixer = require('autoprefixer')
-// const purgecss = require('@fullhuman/postcss-purgecss')
-// const whitelister = require('purgecss-whitelister');
+const purgecss = require('@fullhuman/postcss-purgecss')
+const whitelister = require('purgecss-whitelister');
 
 module.exports = {
   plugins: [
     autoprefixer(),
+    purgecss({
+      content: [
+        './layouts/**/*.html',
+        './content/**/*.md',
+      ],
+      whitelist: [
+        'lazyloaded',
+        'has-text',
+        'first-result',
+        'active',
+        'no-results',
+        'stack-mobile',
+        ...whitelister([
+          './assets/sass/components/_code.scss',
+        ]),
+      ],
+    }),
   ],
 }
