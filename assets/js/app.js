cd02167308ed4f0b767aa364ce590583e149fd4e
Henk Verlinde <henk@ventizo.com>
2022-02-02 10:38:43 +0100

    ux: add scroll position lock for default docs sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,3 +79,16 @@ if (emailForm) {
   })
 }
 /* eslint-enable */
+
+if (document.querySelector('#sidebar-default') !== null) {
+  let sidebar = document.getElementById('sidebar-default');
+
+  let pos = sessionStorage.getItem('sidebar-scroll');
+  if (pos !== null) {
+      sidebar.scrollTop = parseInt(pos, 10);
+  }
+
+  window.addEventListener('beforeunload', () => {
+      sessionStorage.setItem('sidebar-scroll', sidebar.scrollTop);
+  });
+}
