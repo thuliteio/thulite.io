a7a083738afe2a3c34da2090ebd9922a77b85814
Henk Verlinde <henk@ventizo.com>
2022-10-11 17:11:58 +0200

    feat: add temporary plausible exclude option

--- assets/js/plausible.js
+++ assets/js/plausible.js
@@ -5,3 +5,40 @@ const { trackPageview } = Plausible({
 })
 
 trackPageview()
+
+/*
+* Remove when going to production
+* Source: https://plausible.io/docs/excluding-localstorage
+*/
+
+if (document.querySelector('#plausible_button') !== null) {
+window.addEventListener('load', function () {
+  var exclusionState = window.localStorage.plausible_ignore == 'true'
+
+  if (exclusionState) {
+    document.getElementById('plausible_not').style.display = 'none'
+    document.getElementById('plausible_yes').style.display = 'inline'
+    document.getElementById('plausible_button').innerHTML = 'Stop excluding my visits'
+  } else {
+    document.getElementById('plausible_yes').style.display = 'none'
+    document.getElementById('plausible_not').style.display = 'inline'
+    document.getElementById('plausible_button').innerHTML = 'Exclude my visits'
+  }
+});
+
+document.getElementById('plausible_button').addEventListener('click', function(){
+  var exclusionState = window.localStorage.plausible_ignore == 'true'
+
+  if (exclusionState) {
+    delete window.localStorage.plausible_ignore
+    document.getElementById('plausible_yes').style.display = 'none'
+    document.getElementById('plausible_not').style.display = 'inline'
+    document.getElementById('plausible_button').innerHTML = 'Exclude my visits'
+  } else {
+    window.localStorage.plausible_ignore = 'true'
+    document.getElementById('plausible_not').style.display = 'none'
+    document.getElementById('plausible_yes').style.display = 'inline'
+    document.getElementById('plausible_button').innerHTML = 'Stop excluding my visits'
+  }
+});
+}
