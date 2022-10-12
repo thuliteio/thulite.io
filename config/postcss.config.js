c4b58293cf3ab59dffbf8f291611db4310f365b9
Henk Verlinde <henk@ventizo.com>
2022-10-12 09:14:52 +0200

    config: add fw-bold to safelist purgecss

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -27,6 +27,7 @@ module.exports = {
         'offcanvas-backdrop',
         'figcaption',
         'toggle-menu',
+        'fw-bold',
         ...whitelister([
           // './node_modules/@hyas/themes-starter-bootstrap/assets/scss/common/_variables.scss',
           // './node_modules/@hyas/themes-starter-bootstrap/assets/scss/components/_alerts.scss',
