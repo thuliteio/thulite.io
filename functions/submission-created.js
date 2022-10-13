32eb3fb6a44efbeeea3bb9a9b537cd095ec52d92
Henk Verlinde <henk@ventizo.com>
2022-10-13 08:21:26 +0200

    fix: update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,14 +1,31 @@
-import fetch from 'node-fetch'
+require("dotenv").config()
+/*
 import * as dotenv from 'dotenv'
 dotenv.config()
+*/
 
+import fetch from 'node-fetch';
 const { BUTTONDOWN_API_KEY } = process.env
 
 exports.handler = async event => {
-
   const payload = JSON.parse(event.body).payload
-  console.log(payload.email)
+  console.log(`Recieved a submission: ${payload.email}`)
 
+  return fetch("https://api.buttondown.email/v1/subscribers", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: payload.email, notes: payload.name }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(`Submitted to Buttondown:\n ${data}`)
+    })
+    .catch(error => ({ statusCode: 422, body: String(error) }))
+
+  /*
   const rawResponse = await fetch('https://api.buttondown.email/v1/subscribers', {
     method: 'post',
     headers: {
@@ -22,5 +39,6 @@ exports.handler = async event => {
 
     const content = await rawResponse.json();
     console.log(content)
+  */
 
 }
