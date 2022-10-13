04ace983e92fdce09aa821adeba4433fbaecbaad
Henk Verlinde <henk@ventizo.com>
2022-10-13 09:16:50 +0200

    fix: update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,62 +1,38 @@
-// Source: https://leighdinaya.com/blog/create-newsletter-sign-up-buttondown-netlify-functions/
+// Source: https://andrewstiefel.com/netlify-functions-email-subscription/
 
-// require('dotenv').config()
+const { BUTTONDOWN_API_KEY } = process.env
+import fetch from 'node-fetch'
 
+/*
 import * as dotenv from 'dotenv'
 dotenv.config()
-
-// const fetch = require('node-fetch')
-import fetch from 'node-fetch'
-const { BUTTONDOWN_API_KEY } = process.env
+*/
 
 exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' }
+
+  const email = event.queryStringParameters.email || 'No email';
+  console.log(`Received a submission: ${email}`)
+
+  const response = await fetch( 'https://api.buttondown.email/v1/subscribers', {
+      method: 'POST',
+      headers: {
+        Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email }),
+    }
+  );
+
+  let responseText = await response.text();
+  console.log('Response:', responseText);
+
+  /*
+  return {
+    statusCode: 302,
+    headers: {
+        'Location': '/confirmation/',
+    },
   }
+  */
 
-  const errorGen = (msg) => {
-    return { statusCode: 500, body: msg }
-  }
-
-  try {
-    const { email } = JSON.parse(event.body)
-
-    if (!email) {
-      return errorGen('Missing Email')
-    }
-
-    const subscriber = {
-      email,
-    }
-
-    const response = await fetch(
-      'https://api.buttondown.email/v1/subscribers',
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Token ${BUTTONDOWN_API_KEY}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(subscriber),
-      }
-    )
-
-    const data = await response.json()
-    if (!response.ok) {
-      return { statusCode: data.status, body: data.detail }
-    }
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        msg: 'You\'ve signed up to the mailing list!',
-        detail: data,
-      }),
-    }
-  } catch (err) {
-    console.log(err) // output to netlify function log
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: err.message }),
-    }
-  }
-}
+};
