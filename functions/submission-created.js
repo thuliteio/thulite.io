793ced8c3a018af06d2075f35a9eebcd9b57071c
Henk Verlinde <henk@ventizo.com>
2022-10-13 08:43:00 +0200

    fix: update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,44 +1,57 @@
-require("dotenv").config()
-/*
-import * as dotenv from 'dotenv'
-dotenv.config()
-*/
+// Source: https://leighdinaya.com/blog/create-newsletter-sign-up-buttondown-netlify-functions/
 
-import fetch from 'node-fetch';
+require('dotenv').config()
+const fetch = require('node-fetch')
 const { BUTTONDOWN_API_KEY } = process.env
 
-exports.handler = async event => {
-  const payload = JSON.parse(event.body).payload
-  console.log(`Recieved a submission: ${payload.email}`)
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
 
-  return fetch("https://api.buttondown.email/v1/subscribers", {
-    method: "POST",
-    headers: {
-      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email: payload.email, notes: payload.name }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(`Submitted to Buttondown:\n ${data}`)
-    })
-    .catch(error => ({ statusCode: 422, body: String(error) }))
+  const errorGen = (msg) => {
+    return { statusCode: 500, body: msg }
+  }
 
-  /*
-  const rawResponse = await fetch('https://api.buttondown.email/v1/subscribers', {
-    method: 'post',
-    headers: {
-      Authorization: `Token ${BUTTONDOWN_API_KEY}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({email: payload.email}),
-  })
-    .catch(error => ({ statusCode: 422, body: String(error) }))
+  try {
+    const { email } = JSON.parse(event.body)
 
-    const content = await rawResponse.json();
-    console.log(content)
-  */
+    if (!email) {
+      return errorGen('Missing Email')
+    }
 
+    const subscriber = {
+      email,
+    }
+
+    const response = await fetch(
+      'https://api.buttondown.email/v1/subscribers',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(subscriber),
+      }
+    )
+
+    const data = await response.json()
+    if (!response.ok) {
+      return { statusCode: data.status, body: data.detail }
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: 'You\'ve signed up to the mailing list!',
+        detail: data,
+      }),
+    }
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }),
+    }
+  }
 }
