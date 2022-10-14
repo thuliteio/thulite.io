81dba53681b6f6737a3cebaef799689ce7098239
Henk Verlinde <henk@ventizo.com>
2022-10-14 11:26:43 +0200

    feat: update function newsletter for EmailOctopus

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,6 +1,6 @@
 // Source: https://andrewstiefel.com/netlify-functions-email-subscription/
 
-const { EMAILOCTOPUS_API_KEY } = process.env
+// const { EMAILOCTOPUS_API_KEY } = process.env
 import fetch from 'node-fetch'
 
 /*
@@ -13,13 +13,13 @@ exports.handler = async (event) => {
   // const email = event.queryStringParameters.email || 'No email';
   const email = JSON.parse(event.body).payload.email || 'No email';
   console.log(`Received a submission: ${email}`)
-  const response = await fetch( 'https://emailoctopus.com/api/1.6/lists/{{EMAILOCTOPUS_LIST_ID}}/contacts', {
+  const response = await fetch( `https://emailoctopus.com/api/1.6/lists/${EMAILOCTOPUS_LIST_ID}/contacts`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        api_key: EMAILOCTOPUS_API_KEY,
+        api_key: ${EMAILOCTOPUS_API_KEY},
         email_address: email,
       }),
     }
