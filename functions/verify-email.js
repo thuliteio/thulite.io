ae6973f8fddba776bf8384ba8d5432ee9136fcfb
Henk Verlinde <henk@ventizo.com>
2022-10-19 14:39:34 +0200

    feat: add function verify-email.js

---
+++ functions/verify-email.js
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch'
+
+const { EMAILLISTVERIFY_API_KEY } = process.env;
+
+exports.handler = async (event) => {
+
+  const body = JSON.parse(event.body);
+  const { email } = body.payload.data;
+
+  console.log(`Submission: ${email}`)
+
+  let headersList = {
+    'Accept': '*/*',
+  }
+
+  let response = await fetch(`https://apps.emaillistverify.com/api/verifyEmail?secret=${EMAILLISTVERIFY_API_KEY}&email=${email}&timeout=15`, {
+    method: 'GET',
+    headers: headersList,
+  });
+
+  let data = await response.text();
+  console.log('Response:', data);
+
+  return {
+    statusCode: response.status,
+    body: data,
+  };
+
+};
