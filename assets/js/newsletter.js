73a731687eff97d1a9a2ee5bcb405357be5dada9
Henk Verlinde <henk@ventizo.com>
2022-10-21 13:05:22 +0200

    fix: update newsletter.js

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -7,11 +7,11 @@ const processForm = form => {
   const data = new FormData(form)
   data.append('form-name', 'newsletter');
 
+  /*
   const email = data.get('email');
   console.log (email);
-  validateForm()
+  */
 
-  /*
   fetch('/', {
     method: 'POST',
     body: data,
@@ -23,20 +23,6 @@ const processForm = form => {
     form.innerHTML = `<p class="form-error text-center text-md-end mb-0">Oops. Something\\'s wrong: ${error}</p>`;
     // form.innerHTML = `<p class="form-error text-center text-md-end mb-0">Email address verification failed.</p>`;
   })
-  */
-}
-
-async function validateForm(email) {
-  const url = `/functions/validate-email?email=${email}`;
-  try {
-    const response = await fetch(url);
-    const data = await response;
-    console.log(data);
-    return data;
-  }
-  catch (err) {
-    console.log(err);
-  }
 }
 
 const emailForm = document.querySelector('.email-form')
