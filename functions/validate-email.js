7afe8559ec5685fecba1c6f96d42eba577186821
Henk Verlinde <henk@ventizo.com>
2022-10-21 13:43:46 +0200

    fix: update validate-email.js

--- functions/validate-email.js
+++ functions/validate-email.js
@@ -1,26 +1,16 @@
 import fetch from 'node-fetch'
 
 const { EMAILLISTVERIFY_API_KEY } = process.env;
+const API_ENDPOINT = 'https://apps.emaillistverify.com/api/verifyEmail';
 
 exports.handler = async (event) => {
-
   const email = event.queryStringParameters.email
-  console.log(`Submission: ${email}`)
 
-  let headersList = {
-    'Accept': '*/*',
-    }
-
-  let response = await fetch(`https://apps.emaillistverify.com/api/verifyEmail?secret=${EMAILLISTVERIFY_API_KEY}&email=${email}&timeout=30`, {
-    method: 'GET',
-    headers: headersList,
-  });
-
-  let data = await response.text();
-  console.log('Response:', data);
-
-  return {
-    statusCode: response.status,
-    body: data,
-  };
+  return fetch(`${{API_ENDPOINT}}?secret=${EMAILLISTVERIFY_API_KEY}&email=${email}&timeout=30`, { headers: { 'Accept': '*/*' } })
+    .then((response) => response())
+    .then((data) => ({
+      statusCode: 200,
+      body: data,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
