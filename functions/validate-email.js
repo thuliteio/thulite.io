359dd63f2fd38d816c6812e1c388a5b175517d5a
Henk Verlinde <henk@ventizo.com>
2022-10-21 18:08:40 +0200

    fix: processing newsletter form

--- functions/validate-email.js
+++ functions/validate-email.js
@@ -4,36 +4,22 @@ const { EMAILLISTVERIFY_API_KEY } = process.env;
 
 exports.handler = async (event) => {
 
-  if (event.headers.referer.includes('netlify.app')) {
+  const EMAIL = event.queryStringParameters.email
 
-    const EMAIL = event.queryStringParameters.email
+  if (EMAIL) {
+    console.log(`Submission: ${EMAIL}`)
 
-    if (EMAIL) {
-      console.log(`Submission: ${EMAIL}`)
+    const EMAILLISTVERIFY_API = `https://apps.emaillistverify.com/api/verifyEmail?secret=${EMAILLISTVERIFY_API_KEY}&email=${EMAIL}&timeout=30`
 
-      const EMAILLISTVERIFY_API = `https://apps.emaillistverify.com/api/verifyEmail?secret=${EMAILLISTVERIFY_API_KEY}&email=${EMAIL}&timeout=30`
+    const response = await fetch(EMAILLISTVERIFY_API)
+    const status = await response.text()
+    console.log('Response:', status);
 
-      const response = await fetch(EMAILLISTVERIFY_API)
-      const status = await response.text()
-      console.log('Response:', status);
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          status,
-        }),
-      }
-
-    } else {
-
-      return {
-        statusCode: 403,
-        body: JSON.stringify({
-          'status' : 403,
-          'message' : 'Forbidden',
-        }),
-      };
-
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        status,
+      }),
     }
 
   } else {
