258f1c8dc547a2ef1ab0ec88df7189f6b1c73453
Henk Verlinde <henk@ventizo.com>
2022-11-05 18:24:40 +0100

    ux: update homepage

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -3,6 +3,7 @@ import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import json from 'highlight.js/lib/languages/json';
 import bash from 'highlight.js/lib/languages/bash';
+import shell from 'highlight.js/lib/languages/shell';
 import xml from 'highlight.js/lib/languages/xml';
 import ini from 'highlight.js/lib/languages/ini';
 import yaml from 'highlight.js/lib/languages/yaml';
@@ -12,6 +13,7 @@ import scss from 'highlight.js/lib/languages/scss';
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('shell', shell);
 hljs.registerLanguage('html', xml);
 hljs.registerLanguage('ini', ini);
 hljs.registerLanguage('toml', ini);
@@ -21,7 +23,7 @@ hljs.registerLanguage('scss', scss);
 hljs.registerLanguage('txt', bash);
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('pre code').forEach((block) => {
-    hljs.highlightElement(block);
+  document.querySelectorAll('pre code').forEach((el) => {
+    hljs.highlightElement(el);
   });
 });
